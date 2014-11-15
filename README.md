7594b2453498fb58834241410a5afa6dec38b80b
Victor Neo <icyisamu@gmail.com>
2014-11-15 15:18:18 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-enkindle
+Enkindle
 ========
+
+Flask web-app to display milestone deadline and burndown chart for the current sprint.
+
+Coming soon.
+
+![Preview](https://raw.github.com/victorneo/enkindle/master/preview.png)
---
+++ enkindle/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+app.DEBUG = True
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
